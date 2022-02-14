8e5890c7c571022b03df3738bfd7c5db112ecd4f
mariabonnemaison <mariajbonn@gmail.com>
2022-02-14 02:45:42 -0500

    Javascript

---
+++ math.js
@@ -0,0 +1,15 @@
+function greaterThanTen(num) {
+
+    if (num > 10) {
+      console.log("greater than 10!");
+    }
+    else {
+      console.log("less than 10!");
+    }
+
+    return num > 10;
+
+  }
+
+  // this should return true
+  console.log(greaterThanTen(20));
